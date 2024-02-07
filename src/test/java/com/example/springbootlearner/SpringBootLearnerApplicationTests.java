5ec8ffc14b62eb52a7251ddc7e502667f348fed8
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-02-07 15:10:14 +0530

    initial comment

---
+++ src/test/java/com/example/springbootlearner/SpringBootLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
